7665cdc049e20b659c805aab53a4ccc2137a8b2d
Pablo Ramos <ramos.e.pablo@gmail.com>
2019-02-04 23:12:35 -0500

    Added a home view

--- HoosRiding/HoosRiding/settings.py
+++ HoosRiding/HoosRiding/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'HoosRiding.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+              os.path.join(BASE_DIR, 'HoosRiding/templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
