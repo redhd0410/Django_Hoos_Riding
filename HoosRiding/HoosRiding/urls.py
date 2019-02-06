adcef12a813335afd536314aa27a5c86b3b7cfbb
jc4va <31323449+jc4va@users.noreply.github.com>
2019-02-06 17:45:05 -0500

    Update urls.py

--- HoosRiding/HoosRiding/urls.py
+++ HoosRiding/HoosRiding/urls.py
@@ -23,14 +23,23 @@ urlpatterns = [
     # Post & Get User
     path('api/users', views.user),
     path('api/users/<int:pk>', views.user),
+    path('api/users/<int:pk>/update'),
+    path('api/users/<int:pk>/delete'),
+    path('api/users/create'),
 
     # Post & Get Vehicle
     path('api/vehicles', views.vehicle),
     path('api/vehicles/<int:pk>', views.vehicle),
+    path('api/vehicles/<int:pk>/update'),
+    path('api/vehicles/<int:pk>/delete'),
+    path('api/vehicles/create'),
 
     # Post & Get Rides
     path('api/rides', views.ride),
-    path('api/rides<int:pk>', views.ride),
+    path('api/rides/<int:pk>', views.ride),
+    path('api/rides/<int:pk>/update', views.ride),
+    path('api/rides/<int:pk>/delete', views.ride),
+    path('api/rides/create', views.ride),
 
     path('admin/', admin.site.urls),
 ]
