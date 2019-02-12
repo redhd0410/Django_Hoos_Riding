37dcbb586dc5d925e1d4619f27629d6f872ae54f
jc4va <31323449+jc4va@users.noreply.github.com>
2019-02-11 19:41:11 -0500

    Add files via upload

---
+++ HoosRiding/HoosRiding/tests/fruit.py
@@ -0,0 +1,10 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+
+
+
+class GetTestCase(TestCase):
+    def test_something(self):
+        self.assertEquals(1, 2)
+    
+
