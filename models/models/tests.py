9b4f473d5d50fcfc82a37494d858c70bc898537f
jc4va <31323449+jc4va@users.noreply.github.com>
2019-02-19 23:24:52 -0500

    Add files via upload
    Not done yet

---
+++ models/models/tests.py
@@ -0,0 +1,74 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+import urllib.request
+import urllib.parse
+import json
+from models.models import User, Vehicle, Ride
+
+"""
+Questions: 
+
+- How do I handle expected exceptions
+- How do I destroy instances from setup 
+
+"""
+
+class UserTestCase(TestCase):
+
+    def setUp(self):
+        User.objects.create(first_name="Jiwon", last_name="Cha", phone_number="1209381092", profile_url="www.google.com", id = 1)
+        User.objects.create(first_name="Pablo", last_name="Ramos", phone_number="12381274", profile_url="www.google.com", id = 2)
+
+    def test_user_get_request_param(self):
+        response = self.client.get(reverse('user_result_id', kwargs={'pk':1}))
+        self.assertContains(response, 'first_name')
+
+    def test_user_get_request_object(self):
+        response = self.client.get(reverse('user_result_id', kwargs={'pk':1}))
+        self.assertContains(response, "Jiwon")
+
+    def test_user_post_request(self):
+        data = {
+            "phone_number": "121283789",
+            "first_name": "Hello",
+            "profile_url": "www.google.com",
+            "last_name": "Kitty"
+            }
+        response = self.client.post(reverse('user_result_id', kwargs={'pk':3}), data, content_type="application/json")
+        self.assertContains(response, "Hello")
+
+    def test_user_put_request(self):
+        data = {
+            "phone_number": "121283789",
+            "first_name": "Joan",
+            "profile_url": "www.google.com",
+            "last_name": "Smith"
+            }
+        response = self.client.put(reverse('user_result_id', kwargs={'pk':1}), data, content_type="application/json")
+        self.assertContains(response, "Joan")
+
+class VehicleTestCase(TestCase):
+    def setUp(self):
+        User.objects.create(first_name="Jiwon", last_name="Cha", phone_number="1209381092", profile_url="www.google.com", id = 1)
+        User.objects.create(first_name="Pablo", last_name="Ramos", phone_number="12381274", profile_url="www.google.com", id = 2)
+        Vehicle.objects.create(license_plate="123ABC", model="Toyota", color="yellow", driver=User.objects.get(pk=1), id=1)
+
+    def test_user_get_request_param(self):
+        response = self.client.get(reverse('vehicle_result_id', kwargs={'pk':1}))
+        self.assertContains(response, 'color')
+
+    def test_vehicle_get_request(self):
+        response = self.client.get(reverse('vehicle_result_id', kwargs={'pk':1}))
+        self.assertContains(response, "Toyota")
+
+    def test_vehicle_get_put(self):
+        data = {
+            "license_plate": "983XYZ",
+            "model": "Ford",
+            "color": "Black",
+            "driver": 1, 
+        }
+        response = self.client.put(reverse('vehicle_result_id', kwargs={'pk':1}), data, content_type="application/json")
+        self.assertContains(response, "Ford")
+        
+    
