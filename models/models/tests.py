6d2b8c5bcc75ccf750e9d1d50e7088afe387efab
jc4va <31323449+jc4va@users.noreply.github.com>
2019-02-20 15:26:38 -0500

    Update tests.py

--- models/models/tests.py
+++ models/models/tests.py
@@ -25,7 +25,7 @@ class UserTestCase(TestCase):
 
     def test_user_get_request_object(self):
         response = self.client.get(reverse('user_result_id', kwargs={'pk':1}))
-        self.assertContains(response, "Jiwon")
+        self.assertEqual(response.json()['first_name'], "Jiwon")
 
     def test_user_post_request(self):
         data = {
@@ -35,7 +35,7 @@ class UserTestCase(TestCase):
             "last_name": "Kitty"
             }
         response = self.client.post(reverse('user_result_id', kwargs={'pk':3}), data, content_type="application/json")
-        self.assertContains(response, "Hello")
+        self.assertEqual(response.json()['first_name'], "Hello")
 
     def test_user_put_request(self):
         data = {
@@ -45,7 +45,9 @@ class UserTestCase(TestCase):
             "last_name": "Smith"
             }
         response = self.client.put(reverse('user_result_id', kwargs={'pk':1}), data, content_type="application/json")
-        self.assertContains(response, "Joan")
+        self.assertEqual(response.json()['first_name'], "Joan")
+
+        
 
 class VehicleTestCase(TestCase):
     def setUp(self):
@@ -59,7 +61,7 @@ class VehicleTestCase(TestCase):
 
     def test_vehicle_get_request(self):
         response = self.client.get(reverse('vehicle_result_id', kwargs={'pk':1}))
-        self.assertContains(response, "Toyota")
+        self.assertEqual(response.json()['model'], "Toyota")
 
     def test_vehicle_get_put(self):
         data = {
@@ -69,6 +71,31 @@ class VehicleTestCase(TestCase):
             "driver": 1, 
         }
         response = self.client.put(reverse('vehicle_result_id', kwargs={'pk':1}), data, content_type="application/json")
-        self.assertContains(response, "Ford")
-        
+        self.assertEqual(response.json()['model'], "Ford")
+    
+class RideTestCase(TestCase):
+    def setUp(self):
+        User.objects.create(first_name="Jiwon", last_name="Cha", phone_number="1209381092", profile_url="www.google.com", id = 1)
+        User.objects.create(first_name="Pablo", last_name="Ramos", phone_number="12381274", profile_url="www.google.com", id = 2)
+        User.objects.create(first_name="Angelina", last_name="Jolie", phone_number="98732487", profile_url="www.google.com", id = 3)
+        User.objects.create(first_name="Johnny", last_name="Depp", phone_number="878372384", profile_url="www.google.com", id = 4)
+        Vehicle.objects.create(license_plate="123ABC", model="Toyota", color="yellow", driver=User.objects.get(pk=1), id=1)
+        Ride.objects.create(vehicle=Vehicle.objects.get(pk=1), start="DC", destination="New York", depart_time="02/23/17", seats_offered=3, price=80)
+
+    def test_create_ride(self):
+        data = {
+            "vehicle": 1, 
+            "destination": "Montana", 
+            "start": "Cville", 
+            "depart_time": "11/12/14",
+            "price": "13", 
+            "seats_offered": 4, 
+            "id": 2
+        }   
+        response = self.client.post(reverse('ride_result'), data, content_type="application/json")
+        self.assertEqual(response.json()['start'], "Cville")
+
+    def test_add_passengers_to_ride(self):
+        response = self.client.put(reverse('ride_result_uid', kwargs={'pk':1, 'uid':2}))
+        self.assertContains(response, "success")
     
