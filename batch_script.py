a9440e5f2246a1adb9efb68805532be63fb7a1f3
jc4va <31323449+jc4va@users.noreply.github.com>
2019-04-03 11:38:33 -0400

    forms

--- batch_script.py
+++ batch_script.py
@@ -8,5 +8,5 @@ consumer = KafkaConsumer('ride-listings-topic', group_id='listing-indexer', boot
 for message in consumer: 
     single_value = json.loads((message.value).decode('utf-8'))
     print(single_value)
-    es.index(index='ride-list', doc_type='rides', id=single_value['id'], body=single_value)
+    es.index(index='ride-list', doc_type='rides', id=single_value['vehicle'], body=single_value)
     es.indices.refresh(index="ride-list")
