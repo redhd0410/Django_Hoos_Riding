b7f988ba7af09d7b1d8dab70f98570f14a594911
jc4va <31323449+jc4va@users.noreply.github.com>
2019-04-30 11:30:00 -0400

    selenium tests

--- selenium/selenium_test.py
+++ selenium/selenium_test.py
@@ -1,24 +1,43 @@
+# Run the tests: docker start selenium-test && docker exec -it selenium-test bash -c "python selenium_test.py"
+
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from django.test import TestCase
+import time
 
 class TestSelenium(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Remote(
-        command_executor='http://127.0.0.1:4444/wd/hub',
+        command_executor='http://selenium-chrome:4444/wd/hub',
         desired_capabilities=DesiredCapabilities.CHROME)
-        
-    def test_search_in_python_org(self):
-            driver = self.driver
-            driver.get("http://www.python.org")
-            self.assertIn("Python", driver.title)
-            elem = driver.find_element_by_name("q")
-            elem.send_keys("pycon")
-            elem.send_keys(Keys.RETURN)
-            assert "No results found." not in driver.page_source
+
+    def testCreateAccount(self):
+        driver = self.driver
+        driver.get("http://web:8000/createaccount")
+        assert "Create Your Account" in driver.page_source
+
+    def testLogInPage(self):
+        driver = self.driver
+        driver.get("http://web:8000/login")
+        assert "Log In" in driver.page_source
+
+    def testLogInFunction(self):
+        driver =self.driver
+        driver.get("http://web:8000/login")
+        username = driver.find_element_by_id("id_username")
+        password = driver.find_element_by_id("id_password")
+
+        username.send_keys("A")
+        password.send_keys("A")
+
+        driver.find_element_by_name("submit").click()
+
+        print(driver.current_url)
+
+        pass
 
     def tearDown(self):
         self.driver.close()
